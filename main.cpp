02db8ffd72a4c8d0a686e6883f3fa2d4661d90a4
garrett.jennings@aggiemail.usu.edu <garrett.jennings@aggiemail.usu.edu>
2018-03-14 17:03:35 -0600

    wierd memory problems

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-//Garrett Jennings
-//A01491831
+//Garrett Jennings //A01491831
 //HW4 CMD Shell
 //CS3100
 #include <sys/wait.h>
@@ -10,28 +9,52 @@
 
 int main(void)
 {
-	
-	//forever (until "exit" is entered by the user
+	std::vector<std::string> history;
+		//forever (until "exit" is entered by the user
 	while(true) 
 	{
 		std::cout <<"gash cmd-->"; //G-shell i.e. garretts-shell
 		std::string cmd;
 
-		//read a line of input from the user
+			//read a line of input from the user
 		getline(std::cin, cmd); // at this point, I need to 'tokenize' cmd;
 		std::vector<std::string> tokenCmd;
+		history.push_back(cmd);
 		tokenCmd = tokenize(cmd);
+		
+			// loads the tokenized vector into an char ** array
+			// likely not the most efficient but whatevs
+			// we might decide if the input is a builtin command
+
 	
-		// we might decide if the input is a builtin command
-		// Ill later come back to do a switch statement chain.
-		if (cmd == "exit")
+			
+		if(tokenCmd[0] == "ptime")
+		{
+
+		}
+		else if(tokenCmd[0] == "history")
+		{
+
+		}
+		else if(tokenCmd[0] == "^")
+		{
+
+		}
+		else if (tokenCmd[0] == "exit")
 		{
 			break;
 		}
 		
 		else // if the command is not built in we need to search for a program and fork execvp it
 		{	
-			
+			int numArgs = tokenCmd.size();
+			char** args = new char*[numArgs];
+			for(int i = 0; i < numArgs; i++)
+			{
+				args[i] = (char*) tokenCmd[i].c_str();	
+			}
+	
+	
 			if(fork())
 			{
 				// parent waits for the child to finish
@@ -40,13 +63,6 @@ int main(void)
 			}
 			else
 			{
-				int numArgs = tokenCmd.size();
-				char** args = new char*[numArgs];
-				for(int i = 0; i < numArgs; i++)
-				{
-					args[i] = (char*) tokenCmd[i].c_str();	
-					std::cout << tokenCmd[i];
-				}
 				//child, executes the user's input as a command
 				//char** args = new char*[2];
 				//args[0] = (char*) cmd.c_str();
